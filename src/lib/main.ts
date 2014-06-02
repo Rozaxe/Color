83cf6996ea63a218281986b9b725a75edc6db725
Rozaxe <axel.rozobrezillac@gmail.com>
2014-06-02 11:58:07 -0400

    Change animation colors

--- src/lib/main.ts
+++ src/lib/main.ts
@@ -3,5 +3,7 @@
 /// <reference path="scenes.ts" />
 
 window.onload = function() {
+	// Animation testing
+	//window.setTimeout(() => Managers.Scene.init_scene(new Scenes.Title()), 2000)
 	Managers.Scene.init_scene(new Scenes.Title())
 }
