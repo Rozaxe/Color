aa4e1317dd0f9badb732a0ad838ec2eec27fab53
Rozaxe <axel.rozobrezillac@gmail.com>
2014-06-03 21:36:26 -0400

    Change whitespace

--- src/lib/Managers.ts
+++ src/lib/Managers.ts
@@ -3,42 +3,42 @@
 /// <reference path="tools.ts" />
 
 module Managers {
-    export module Scene {
-        var scene: Scenes.Base
-        var attachs: Attached[] = new Array()
+	export module Scene {
+		var scene: Scenes.Base
+		var attachs: Attached[] = new Array()
 
-        class Attached {
-            constructor(public e: EventTarget, public t: string, public l: (e: Event) => void) { }
-        }
+		class Attached {
+			constructor(public e: EventTarget, public t: string, public l: (e: Event) => void) { }
+		}
 
-        function clean_streams() {
-            attachs.forEach(function(a: Attached) {
-                a.e.removeEventListener(a.t, a.l, false)
-            })
-        }
+		function clean_streams() {
+			attachs.forEach(function(a: Attached) {
+				a.e.removeEventListener(a.t, a.l, false)
+			})
+		}
 
-        export function attach(e: EventTarget, t: string, l: (e: Event) => void): void {
-            e.addEventListener(t, l, false)
-            attachs.push(new Attached(e, t, l))
-        }
+		export function attach(e: EventTarget, t: string, l: (e: Event) => void): void {
+			e.addEventListener(t, l, false)
+			attachs.push(new Attached(e, t, l))
+		}
 
-        export function change_scene(s: Scenes.Base): void {
-            scene.terminate()
-            clean_streams()
-            init_scene(s)
-        }
+		export function change_scene(s: Scenes.Base): void {
+			scene.terminate()
+			clean_streams()
+			init_scene(s)
+		}
 
-        export function init_scene(s: Scenes.Base): void {
-            scene = s
-            scene.start()
-        }
-    }
+		export function init_scene(s: Scenes.Base): void {
+			scene = s
+			scene.start()
+		}
+	}
 
 	export module Color {
 		var colors : Tools.Color[] = new Array()
 
-        //                           Name      Regular    Light
-		colors.push(new Tools.Color('red',    '#c0392b', '#e74c3c'))
+		//						   Name	  Regular	Light
+		colors.push(new Tools.Color('red',	'#c0392b', '#e74c3c'))
 		colors.push(new Tools.Color('green',  '#27ae60', '#2ecc71'))
 		colors.push(new Tools.Color('blue',   '#2980b9', '#3498db'))
 		colors.push(new Tools.Color('yellow', '#f39c12', '#f1c40f'))
--- src/lib/scenes/score.ts
+++ src/lib/scenes/score.ts
@@ -4,41 +4,41 @@
 /// <reference path="color.ts" />
 
 module Scenes {
-    export class Score implements Base {
-        score:  HTMLElement = document.getElementById('score')
-        shadow: HTMLElement = document.getElementById('shadow')
-        num:    HTMLElement = document.getElementById('num')
-        replay: HTMLElement = document.getElementById('restart')
+	export class Score implements Base {
+		score:  HTMLElement = document.getElementById('score')
+		shadow: HTMLElement = document.getElementById('shadow')
+		num:    HTMLElement = document.getElementById('num')
+		replay: HTMLElement = document.getElementById('restart')
 
-        result: number
-        time: number
+		result: number
+		time: number
 
-        constructor(score: number) {
-            this.result = score
-        }
+		constructor(score: number) {
+			this.result = score
+		}
 
-        start() {
-            this.num.innerHTML = this.result.toString()
-            this.shadow.className += ' visible'
-            this.score.className  += ' open'
-            Managers.Scene.attach(this.replay, 'click', this.restart)
-            Managers.Scene.attach(window, 'keyup', (e: KeyboardEvent) => this.restart())
-            var link = <HTMLLinkElement> this.score.querySelector('a')
-            link.href = 'https://twitter.com/share?text=Arriverez-vous à faire mieux que mon score de ' + this.result + ' ? Pour tenter, c\'est pas ici'
-            this.time = (new Date()).getTime()
-        }
+		start() {
+			this.num.innerHTML = this.result.toString()
+			this.shadow.className += ' visible'
+			this.score.className  += ' open'
+			Managers.Scene.attach(this.replay, 'click', this.restart)
+			Managers.Scene.attach(window, 'keyup', (e: KeyboardEvent) => this.restart())
+			var link = <HTMLLinkElement> this.score.querySelector('a')
+			link.href = 'https://twitter.com/share?text=Arriverez-vous à faire mieux que mon score de ' + this.result + ' ? Pour tenter, c\'est pas ici'
+			this.time = (new Date()).getTime()
+		}
 
-        restart() {
-            var now = (new Date()).getTime()
-            if ((now - this.time) < 400) {
-                return
-            }
-            Managers.Scene.change_scene(new Scenes.Color())
-        }
+		restart() {
+			var now = (new Date()).getTime()
+			if ((now - this.time) < 400) {
+				return
+			}
+			Managers.Scene.change_scene(new Scenes.Color())
+		}
 
-        terminate() {
-            this.shadow.className = this.shadow.className.replace(/\s*\bvisible\b/, '')
-            this.score.className  = this.score.className.replace(/\s*\bopen\b/, '')
-        }
-    }
+		terminate() {
+			this.shadow.className = this.shadow.className.replace(/\s*\bvisible\b/, '')
+			this.score.className  = this.score.className.replace(/\s*\bopen\b/, '')
+		}
+	}
 }
